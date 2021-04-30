b034fbafd14cdcb74c4b3f9d6ca98e7faadd53fa
makenaJun <makena.jun@gmail.com>
2021-04-30 20:17:05 +0300

    designed the page Not Found

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,16 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './NotFoundPage.module.scss';
 
 export const NotFoundPage: FC = () => {
-    return (
-        <div>
-          404 Not Found Page
-        </div>
-    )
+  return (
+    <div className={styles.wrappNotFound}>
+      <div className={styles.sign}>
+        <div className={styles.signInner}>404</div>
+      </div>
+      <div className={styles.title} >PAGE NOT FOUND</div>
+      <div className={styles.description}>Страница, которую вы ищете, не существует.</div>
+      <Link to={'/'} className={styles.button}>На главную</Link>
+    </div>
+  )
 };
